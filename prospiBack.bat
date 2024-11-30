ff00fbbb298d3ef734b5237dc03574ca6acf0810
AriasJonathan30s <jacarijon@gmail.com>
2024-11-30 03:22:46 -0600

    fix files

--- prospiBack.bat
+++ prospiBack.bat
@@ -1,10 +1,5 @@
 @echo off
 TITLE Prospi Back
 
-git diff --quiet || (
-    git stash
-    git pull
-    npm i --save )
-
 npm test
 exit
---
+++ prospiBackUpdr.bat
@@ -0,0 +1,5 @@
+@echo off
+TITLE Prospi Back
+
+git diff --quiet || git stash git pull npm i --save
+exit
