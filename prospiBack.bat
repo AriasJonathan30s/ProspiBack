5d380d5cfecc5e28c629841eb59bc73a178e98d4
AriasJonathan30s <jacarijon@gmail.com>
2024-11-30 02:12:09 -0600

    fix commands

--- prospiBack.bat
+++ prospiBack.bat
@@ -1,8 +1,7 @@
 @echo off
 TITLE Prospi Back
 
-git diff --quiet || git stash && git pull origin master
-npm i --save
+git diff --quiet || (git stash && git pull origin master && npm i --save)
 
 start npm test
 exit
